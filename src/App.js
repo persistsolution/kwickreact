c303548bde801663910acfde9209552c1ea29d56
persistsolution <persistsolution1@gmail.com>
2025-01-19 17:24:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Staging
+          Edit <code>src/App.js</code> and save to reload. Stag
         </p>
         <a
           className="App-link"
